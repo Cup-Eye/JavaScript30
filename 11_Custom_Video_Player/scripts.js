51eced27aa21ae5d9f8413c1f3bc2757bce62d0b
Cup-Eye <mail@berkaykabay.de>
2019-12-20 16:48:48 +0100

    all done

--- 11_Custom_Video_Player/scripts.js
+++ 11_Custom_Video_Player/scripts.js
@@ -24,7 +24,16 @@ function skip() {
 
 function handleRangeUpdate() {
   video[this.name] = this.value;
+};
 
+function handleProgress() {
+const percent = (video.currentTime / video.duration) * 100;
+progressBar.style.flexBasis = `${percent}%`;
+};
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
 };
 
 //connect to eventlisteners
@@ -33,7 +42,17 @@ video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 toggle.addEventListener("click", togglePlay);
 
+
 skipButtons.forEach(button => button.addEventListener("click", skip))
 
 ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
 ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+
+video.addEventListener("timeupdate", handleProgress);
+progress.addEventListener("click", scrub);
+
+let mousedown = false;
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
+
